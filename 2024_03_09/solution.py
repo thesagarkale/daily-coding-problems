0c72525a7d99000d77ad4db3d65d058023bb0345
Sagar Kale <sagarkale2020@gmail.com>
2024-03-13 08:40:36 +0000

    Solution for 2024_03_09 updated, for O(Log(N)) without bitwize

--- 2024_03_09/solution.py
+++ 2024_03_09/solution.py
@@ -10,16 +10,16 @@ def divide(dividend, divisor):
     dividend, divisor = abs(dividend), abs(divisor)
 
     quotient = 0
-    remainder = 0
 
-    for i in range(31, -1, -1):
-        if (dividend >> i) >= divisor:
-            quotient += (1 << i)
-            dividend -= (divisor << i)
+    while dividend >= divisor:
+        temp, multiple = divisor, 1
+        while dividend >= temp:
+            dividend -= temp
+            quotient += multiple
+            temp += temp
+            multiple += multiple
 
-    quotient *= sign
+    return quotient, dividend
 
-    return (quotient, remainder)
-
-result = divide(10, 3)
+result = divide(100, 2)
 print(result)
